ba4621533d6eb155ac692dd907f95c3cb726127b
Vikas S Pabale <vikaspabaleit@gmail.com>
2025-07-23 13:04:30 +0530

    Update SQL.sql

--- Coforge/SQL.sql
+++ Coforge/SQL.sql
@@ -1,2 +1,12 @@
 1. How would you find duplicate records in a table and delete only the duplicates while keeping one copy?
+
+  WITH dup_data AS (
+  select col1, col2, col3, email_id, id,
+  ROW_NUMBER() OVER(partition by email_id order by id ) as row_num 
+  from employee
+  )
+  select col1, col2, col3, email_id, id from dup_data where row_num = 1
+  
 2. A table has a start_date and end_date column. How would you calculate the total number of overlapping days across all rows?
+
+
